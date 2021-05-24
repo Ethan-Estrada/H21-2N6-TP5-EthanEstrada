84f2ca4dbc2c32a499d8ba86ea67369e82c5e986
Ethan Estrada <2056092@cegepmonpetit.ca>
2021-05-23 22:10:36 -0400

    FCT : TODO#6 Ajout de la classe ChansonWMA

---
+++ R24TP05/BaladeurMultiFormats/ChansonWMA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormats
+{
+    public class ChansonWMA : Chanson
+    {
+        public override string Format => throw new NotImplementedException();
+
+        public override void EcrireEntete(StreamReader pobjFichier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EcrireParoles(StreamWriter pobjFichier, string pParoles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LireEntete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string LireParoles(StreamReader pobjFichier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
